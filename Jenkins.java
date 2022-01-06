48127c1470a3e997e25b18e4e98257dcc2193d24
Bart <bw2645@gmail.com>
2022-01-06 14:24:51 +0000

    Add files via upload

---
+++ Jenkins.java
@@ -0,0 +1,7 @@
+public class Jenkins {
+  public static void main(String[] args) {
+    System.out.println("Name: Bartosz");
+    System.out.println("Surname: Wieloch");
+    System.out.println("Job Role: Student Technical");
+  }
+}
